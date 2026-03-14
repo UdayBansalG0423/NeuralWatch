90f1e6c84f65d030e22869ee68e52082f2856dc6
UdayBansalG0423 <bansaluday.112@gmail.com>
2026-03-14 23:44:41 +0530

    feat: store telemetry logs in PostgreSQL

--- backend/app/api/logs.py
+++ backend/app/api/logs.py
@@ -1,11 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 from app.schemas.log_schema import TelemetryLog
+from app.db.session import SessionLocal
+from app.models.log_models import RequestLog
 
 router = APIRouter(prefix="/log", tags=["telemetry"])
 
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @router.post("/")
-def ingest_log(log: TelemetryLog):
-    return {
-        "message": "log received",
-        "data": log
-    }
+def ingest_log(log: TelemetryLog, db: Session = Depends(get_db)):
+
+    new_log = RequestLog(**log.dict())
+
+    db.add(new_log)
+    db.commit()
+    db.refresh(new_log)
+
+    return {"message": "log stored"}
