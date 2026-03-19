a145dd7984287dcbc6d4c63f09429e78bb7ec81c
UdayBansalG0423 <bansaluday.112@gmail.com>
2026-03-19 10:09:23 +0530

    feat: generate initial migration for request_logs table

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -21,7 +21,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from app.db.base import Base
-from app.models import log_model
+from app.models import log_models
 
 target_metadata = Base.metadata
 
