e3f211c4f16033ecb682de8ac1786a935e7dccde
UdayBansalG0423 <bansaluday.112@gmail.com>
2026-03-25 15:02:50 +0530

    feat: initialize metrics engine module

---
+++ backend/app/metrices/engine.py
@@ -0,0 +1,16 @@
+class MetricsEngine:
+
+    def __init__(self, db):
+        self.db = db
+
+    def get_overview(self):
+        pass
+
+    def get_latency_metrics(self):
+        pass
+
+    def get_cost_metrics(self):
+        pass
+
+    def get_reliability_score(self):
+        pass
